f253b66d76b61d51e0be0c59decffa1c1e3c7a94
Natanael Rodrigues <natanaelrodrigues@gmail.com>
2023-05-29 15:14:50 -0300

    resolve bug compile

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,21 +1,22 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-@Entity({ name: 'users' })
+
+@Entity({ name: 'user' })
 export class UserEntity {
   @PrimaryGeneratedColumn('rowid')
   id: number;
 
-  @Column({ name: 'name', nulable: false })
+  @Column({ name: 'name', nullable: false })
   name: string;
 
-  @Column({ name: 'email', nulable: false })
+  @Column({ name: 'email', nullable: false })
   email: string;
 
   @Column({ name: 'phone' })
   phone: string;
 
-  @Column({ name: 'cpf', nulable: false })
+  @Column({ name: 'cpf', nullable: false })
   cpf: string;
 
-  @Column({ name: 'password', nulable: false })
+  @Column({ name: 'password', nullable: false })
   password: string;
 }
