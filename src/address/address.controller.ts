f6fb310e0cfc87dfdaa84203150078d0a9989af7
Natanael Rodrigues <natanaelrodrigues@gmail.com>
2023-06-15 19:14:05 -0300

    Create decorator userId

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -5,17 +5,18 @@ import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
 import { UserType } from 'src/user/enum/user-type.enum';
 import { Roles } from 'src/decorators/roles.decorator';
+import { UserId } from 'src/decorators/user-id.decorator';
 
 @Roles(UserType.User)
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
-  @Post('/:userId')
+  @Post('')
   @UsePipes(ValidationPipe)
   async createAddress(
     @Body() createAddressDto: CreateAddressDto,
-    @Param('userId') userId: number,
+    @UserId('') userId: number,
   ): Promise<AddressEntity> {
     return this.addressService.createAddress(createAddressDto, userId);
   }
---
+++ src/utils/base-64-converter.ts
@@ -0,0 +1,15 @@
+import { LoginPayload } from 'src/auth/dtos/loginPayload.dto';
+
+export const authorizationToLoginPayload = (
+  authorization: string,
+): LoginPayload | undefined => {
+  const authorizationSpleted = authorization.split('.');
+
+  if (authorizationSpleted.length < 3 || !authorizationSpleted[1]) {
+    return undefined;
+  }
+
+  return JSON.parse(
+    Buffer.from(authorizationSpleted[1], 'base64').toString('ascii'),
+  );
+};
