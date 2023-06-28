a97fa2fd44bfef7516255225bdd9fa0015bb55ed
Natanael Rodrigues <natanaelrodrigues@gmail.com>
2023-06-28 15:24:42 -0300

    create categoryService

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { CategoryEntity } from './entities/category.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private readonly categoryRepository: Repository<CategoryEntity>,
+  ) {}
+
+  async findAllCaltegories(): Promise<CategoryEntity[]> {
+    const categories = await this.categoryRepository.find();
+
+    if (!categories || categories.length === 0) {
+      throw new NotFoundException('Categories empty');
+    }
+    return categories;
+  }
+}
